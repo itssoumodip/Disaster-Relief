8565dbf3528b09eb56280ca4e708fb949140570c
Soumodip Das <soumodipddas2005@gmail.com>
2025-02-22 23:48:29 +0000

    Remove Navigation and WeatherPage components from App; clean up imports

--- disaster-predictor-project/src/App.jsx
+++ disaster-predictor-project/src/App.jsx
@@ -5,8 +5,6 @@ import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import CreateAccount from './components/CreateAccount';
 import NewsPage from './components/NewsPage';
-import Navigation from './components/Navigation';
-import WeatherPage from './components/WeatherPage';
 import './App.css';
 
 const App = () => {
@@ -15,13 +13,11 @@ const App = () => {
       <div className="min-h-screen flex flex-col">
         <Header />
         <div className="flex-grow">
-          <Navigation />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/create-account" element={<CreateAccount />} />
             <Route path="/news" element={<NewsPage />} />
-            <Route path="/weather" element={<WeatherPage />} />
           </Routes>
         </div>
       </div>
